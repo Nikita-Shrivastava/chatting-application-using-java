8dec99b1d1b88ac86ae926aa41cb16f3b9802e64
Nikita Shrivastava <nikitashrivastava0313@gmail.com>
2025-10-27 18:13:30 +0530

    Adding Files

---
+++ Server.java
@@ -0,0 +1,208 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.io.*;
+
+public class Server implements ActionListener{
+  JTextField text;
+  JPanel a1;
+   static DataOutputStream dout;
+   static Box vertical = Box.createVerticalBox();
+    static JFrame f = new JFrame();
+
+    Server(){
+
+      f.setLayout(null);
+      JPanel p1 = new JPanel();
+      p1.setBackground(new Color(7, 94, 84));
+      p1.setBounds(0, 0, 450,70);
+      p1.setLayout(null);
+      f.add(p1);
+
+     ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("src/icons/icons/3.png"));
+     Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+     ImageIcon i3 = new ImageIcon(i2);
+     JLabel back = new JLabel(i3);
+     back.setBounds(5, 20, 25,25);
+     p1.add(back);
+
+     back.addMouseListener(new MouseAdapter(){
+      public void mouseClicked(MouseEvent ae){
+        System.exit(0);
+      }
+     });
+
+     ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("src/icons/icons/1.png"));
+     Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+     ImageIcon i6 = new ImageIcon(i5);
+     JLabel profile = new JLabel(i6);
+     profile.setBounds(40, 10, 50, 50);
+     p1.add(profile);
+
+     ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("src/icons/icons/video.png"));
+     Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+     ImageIcon i9 = new ImageIcon(i8);
+     JLabel video = new JLabel(i9);
+     video.setBounds(300, 20, 30, 30);
+     p1.add(video);
+
+     ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("src/icons/icons/phone.png"));
+     Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+     ImageIcon i12 = new ImageIcon(i11);
+     JLabel phone = new JLabel(i12);
+     phone.setBounds(360, 20, 30, 30);
+     p1.add(phone);
+
+     ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("src/icons/icons/3icon.png"));
+     Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
+     ImageIcon i15 = new ImageIcon(i14);
+     JLabel morevert = new JLabel(i15);
+     morevert.setBounds(420, 20, 10, 25);
+     p1.add(morevert);
+
+     JLabel name = new JLabel("Gaitonde");
+     name.setBounds(110, 15, 100, 18);
+     name.setForeground(Color.WHITE);
+     name.setFont(new Font("SAN SERIF", Font.BOLD,18));
+     p1.add(name);
+
+     JLabel status = new JLabel("Active Now");
+     status.setBounds(110, 35, 100, 18);
+     status.setForeground(Color.WHITE);
+     status.setFont(new Font("SAN SERIF", Font.BOLD,14));
+     p1.add(status);
+
+     // TEXT AREA PANEL
+
+     a1 = new JPanel();
+     a1.setBounds(5, 75, 440, 570);
+     f.setUndecorated(true);
+     f.add(a1);
+     
+     // USER TEXT FIELD
+
+     text = new JTextField();
+     text.setBounds(5,655, 310,40);
+     text.setFont(new Font("SAN_SARIF",Font.PLAIN,16));
+     f.add(text);
+
+     //ADD BUTTON
+
+     JButton  send = new JButton("Send");
+     send.setBounds(320, 655, 123, 40);
+     send.setBackground(new Color(7, 94, 84));
+     send.setForeground(Color.WHITE);
+     send.addActionListener(this);
+     send.setFont(new Font("SAN_SARIF",Font.PLAIN,16));
+     f.add(send);
+
+      f.setSize(450, 700);
+      f.setLocation(200,50);
+      f.getContentPane().setBackground(Color.WHITE);
+      f.setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent ae){
+     try{ 
+      String out = text.getText();
+  
+      
+      JPanel p2 = formatLabel(out);
+      
+
+      a1.setLayout(new BorderLayout());
+
+      JPanel right = new JPanel(new BorderLayout());
+      right.add(p2, BorderLayout.LINE_END);
+      vertical.add(right);
+      vertical.add(Box.createVerticalStrut(15));
+
+      a1.add(vertical, BorderLayout.PAGE_START);
+      if (dout != null) {
+    dout.writeUTF(out);
+    } else {
+    System.out.println("⚠️ No client connected yet!");
+     }
+
+
+      text.setText("");
+
+
+      f.repaint();
+      f.invalidate();
+      f.validate();
+     }catch(Exception e){
+      e.printStackTrace();
+
+     }
+    }
+
+    // msg ko format krne ke liye proper box hona chahiye jpanel bnaye hai usme direct formatLabel ka object bna kr output arguement de denge
+    public static JPanel formatLabel(String out){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel output = new JLabel("<html><p style=\"width: 150px\" >"+ out + "</html>");
+        output.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        output.setBackground(new Color(37, 211, 102));
+        output.setOpaque(true);
+        output.setBorder(new EmptyBorder(15,15,15, 50));
+        panel.add(output);
+
+        //SHOW FOR TIMING
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+
+         //frmae ke andr kuch bhi likhna hai to uske liye jlabel use krenge or timing show krne ke liye jlabel ka object bnana pdega..
+
+         JLabel time = new JLabel();
+         time.setText(sdf.format(cal.getTime()));
+
+         panel.add(time);
+        return panel;
+    }
+
+
+
+
+    public static void main(String[] args){
+      new Server(); // anonymous Object
+
+      try{
+         ServerSocket skt = new ServerSocket(6001);
+         while (true) { 
+             Socket s = skt.accept();
+             // data receive krna hai uske liye ek class hoti hai datainputstream krke
+             DataInputStream din = new DataInputStream(s.getInputStream());
+
+             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+             //Infinite data receive or send krna hai uske liye ek protocol use hota hai- readUTF()
+             while (true) { //received message
+                 String msg = din.readUTF();
+                 //is msg ko frame me display krna hai uske liye
+                 a1.setLayout(new BorderLayout());
+                 JPanel panel = formatLabel(msg);
+
+                 JPanel left = new JPanel(new BorderLayout());
+                 left.add(panel, BorderLayout.LINE_START);
+                 a1.add(vertical, BorderLayout.PAGE_START);
+                 vertical.add(left);
+                 f.validate(); 
+             }
+
+         }
+
+      }catch (Exception e){
+          e.printStackTrace();
+      }
+    }
+}
+// frame ko reload krna hoga taaki jb msg send kre to wo show ho righr side- repaint(), invalidate(), validate()
+//server socket ek hote hai pr sockets multiple ho skte hai....
